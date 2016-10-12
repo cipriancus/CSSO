e5f0d3811f2691c6e616cf4bf83ed10359484c87
cipriancus <cipriancus@gmail.com>
2016-10-12 19:26:55 +0300

    Lab1CSSO RC

--- Tema1CSSO/Tema1CSSO/Source.cpp
+++ Tema1CSSO/Tema1CSSO/Source.cpp
@@ -2,7 +2,15 @@
 #include<cstring>
 #include<cstdlib>
 #include<iostream>
+#include <tchar.h> 
+#include <stdio.h>
+#include <strsafe.h>
+#include <wchar.h>
 using namespace std;
+
+#pragma comment(lib, "User32.lib")
+
+char path[] = "D:\\Filme\\*";
 
 wchar_t *convertCharArrayToLPCWSTR(const char* charArray)
 {
@@ -11,61 +19,119 @@ wchar_t *convertCharArrayToLPCWSTR(const char* charArray)
 	return wString;
 }
 
-char *returnOnlyName(wchar_t *path) {
-	char new_path[100];
+char *returnOnlyName(const wchar_t *path) {
+	char new_path[100000];
 	int i;
 	wcstombs(new_path, path,sizeof(new_path));
 	
-
-	for (i = strlen(new_path) - 1; i >= 0; i--) {
+	for (i =0; i< strlen(new_path) - 1; i++) {
 		if (new_path[i] == '\\')
 			break;
 	}
-	return strcpy(new_path, new_path + i + 1);;
-}
 
-void createRegEntry(wchar_t *path) {
-	HKEY hkey;
-	char new_path[100]= "SOFTWARE\\CSSO\\";
+	strcpy(new_path, new_path + i + 1);
 
-	strcat(new_path,returnOnlyName(path));
-	
-	RegCreateKeyEx(HKEY_CURRENT_USER, convertCharArrayToLPCWSTR(new_path), 0, 0, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hkey, NULL);
-}
-
-boolean parseFolder(HANDLE handle) {
-	for () {
-		//parsam toate file-urile
-		if()//daca e fisier parseFolder(path)==false;break si eroare
-		else
-			//daca e file createRegEntry(path);
+	for (i = strlen(new_path) - 1; i>=0 ; i--) {
+		if (new_path[i] == '\\')
+			break;
 	}
-	return true;
+
+	strcpy(new_path + i, new_path + i + 2);
+
+	return new_path;
+}
+
+boolean createRegEntryFolder(const wchar_t *path) {
+	HKEY hkey;
+	wstring new_path = wstring((const wchar_t*)L"SOFTWARE\\CSSO\\") + wstring(convertCharArrayToLPCWSTR(returnOnlyName(path)));	
+	const wchar_t *npath = new_path.c_str();
+
+	if (RegCreateKeyEx(HKEY_CURRENT_USER, npath, 0, 0, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hkey, NULL) ==0)
+		return true;
+
+	return false;
+}
+
+boolean createRegEntryFile(const wchar_t *path,const wchar_t *fileName,const BYTE *filesize) {
+	HKEY hkey;
+	wstring folder_path = wstring((const wchar_t*)L"SOFTWARE\\CSSO\\") + wstring(convertCharArrayToLPCWSTR(returnOnlyName(path)));
+	folder_path.pop_back();
+	folder_path.pop_back();
+	const wchar_t *npath = folder_path.c_str();
+
+	LONG openHandle = RegOpenKeyEx(HKEY_CURRENT_USER, npath, 0, KEY_ALL_ACCESS, &hkey);
+	if (openHandle == 0) {
+		if (RegSetValueEx(hkey, fileName, 0, REG_DWORD, filesize, sizeof(filesize)) == 0)
+			return true;
+		return false;
+	}
+	return false;
+}
+
+
+boolean parseFolder(const wchar_t *path) {
+	WIN32_FIND_DATA ffd;
+	HANDLE handle = FindFirstFile(path, &ffd);//"."
+	FindNextFile(handle, &ffd);//".."
+
+	if (handle != INVALID_HANDLE_VALUE) {
+		if (FindNextFile(handle, &ffd) == 0) {
+			return true;
+		}
+		else {
+			do
+			{
+				wstring newpath = wstring(path).substr(0, wstring(path).length() - 1);
+				if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+				{
+					wstring newpath = wstring(path).substr(0, wstring(path).length() - 1) + wstring(ffd.cFileName) + L"\\*";
+					//daca esueaza un folder ne oprim
+					if (createRegEntryFolder(newpath.c_str()) == false) {
+						cout << "Path-ul " << newpath.c_str() << " a esuat" << endl;
+						return false;
+					}
+
+					//daca esueaza un path ne oprim
+					if (parseFolder(newpath.c_str()) == false) {
+						cout << "Path-ul " << newpath.c_str() << " a esuat" << endl;
+						return false;
+					}
+
+				}
+				else
+				{
+
+					/*LARGE_INTEGER filesize;
+
+					filesize.LowPart = ffd.nFileSizeLow;
+					filesize.HighPart = ffd.nFileSizeHigh;
+
+					(BYTE)(filesize.QuadPart*(MAXDWORD + 1))*/
+
+
+					//daca esueaza un file ne oprim
+					if (createRegEntryFile(newpath.c_str(), wstring(ffd.cFileName).c_str(), ) == false) {
+						cout << "Path-ul " << newpath.c_str() << " a esuat" << endl;
+						return false;
+					}
+
+				}
+			} while (FindNextFile(handle, &ffd) != 0);
+			//CloseHandle(handle);
+			return true;
+		}
+	}
+	CloseHandle(handle);
+	return false;
 }
 
 int main() {
-	char path[] = "D:\\Muzicile";
 
-	HANDLE handle = CreateFile(convertCharArrayToLPCWSTR(path), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
-	
-	if (handle == INVALID_HANDLE_VALUE) {
-		cout << "Path invalid"<<endl;
-		
-		if (GetLastError() == ERROR_FILE_NOT_FOUND){
-			//boolean flag = CreateDirectory(convertCharArrayToLPCWSTR(path), NULL);
-			//createRegEntry(convertCharArrayToLPCWSTR(path));
-		}
-
-	}
-	else {//parcurgere recursiva
-		if (parseFolder(handle) == 1)
+	if (parseFolder(convertCharArrayToLPCWSTR(path)) == 1)
 			cout << "Operatie reusita" << endl;
-		else
+	else
 			cout << "Operatia a esuat" << endl;
-	}
 
-	cout << GetLastError();
-	CloseHandle(handle);
 	system("pause");
 	return 0;
 }
